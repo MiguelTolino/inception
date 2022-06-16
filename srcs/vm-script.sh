99ef5b2d440fd5445f439f8c407831ee8443da8a
MiguelTolino <mmateo-t@student.42madrid.com>
2022-06-16 20:29:51 +0200

    files added

---
+++ srcs/vm-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y vim git make \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+git clone https://github.com/MikelTolino/dotfiles.git .dotfiles
+sudo docker run hello-world
+cd $HOME/.dotfiles && cp .vimrc .gitconfig $HOME
