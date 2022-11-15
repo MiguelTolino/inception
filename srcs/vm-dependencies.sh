294bb29e7fd046d0a60280001b1ca7e0548f6833
mmateo-t@student.42madrid.com <mmateo-t@c1r11s6.42madrid.com>
2022-11-15 20:12:14 +0100

    Modify VM dependencies (Docker & compilation essential) (fix #1)

--- srcs/vm-dependencies.sh
+++ srcs/vm-dependencies.sh
@@ -6,14 +6,18 @@
 #    By: mmateo-t <mmateo-t@student.42madrid>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/27 12:59:32 by mmateo-t          #+#    #+#              #
-#    Updated: 2022/06/27 12:59:33 by mmateo-t         ###   ########.fr        #
+#    Updated: 2022/11/15 19:30:08 by mmateo-t         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y vim git make \
+# Linux development dependencies
+sudo apt-get update
+sudo apt-get -y install dkms build-essential linux-headers-$VERSION
+
+# Docker installation
+sudo apt install -y vim make \
     ca-certificates \
     curl \
     gnupg \
@@ -23,7 +27,6 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-git clone https://github.com/MikelTolino/dotfiles.git .dotfiles
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo docker run hello-world
-cd $HOME/.dotfiles && cp .vimrc .gitconfig $HOME
+
