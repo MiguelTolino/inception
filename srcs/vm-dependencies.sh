faa83eebfef297d7c981d4fd1387354f34eb73c6
mmateo-t@student.42madrid.com <mmateo-t@c1r11s6.42madrid.com>
2022-11-15 20:12:31 +0100

    Modify VM dependencies (Docker & compilation essential) (fix #1)

--- srcs/vm-dependencies.sh
+++ srcs/vm-dependencies.sh
@@ -29,4 +29,3 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo docker run hello-world
-
