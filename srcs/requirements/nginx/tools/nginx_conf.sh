44a36fa52fad5a83e1dfc2a8dab61124547c242b
mmateo-t <mmateo-t@c1r6s3.42madrid.com>
2022-11-24 18:44:07 +0100

    Modifications in Nginx (ref #5)

--- srcs/requirements/nginx/tools/nginx_conf.sh
+++ srcs/requirements/nginx/tools/nginx_conf.sh
@@ -6,7 +6,7 @@
 #    By: mmateo-t <mmateo-t@student.42madrid>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/27 13:04:47 by mmateo-t          #+#    #+#              #
-#    Updated: 2022/07/06 12:53:15 by mmateo-t         ###   ########.fr        #
+#    Updated: 2022/11/24 18:43:19 by mmateo-t         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,8 +18,9 @@ mkdir /www
 chown -R www:www /var/lib/nginx
 chown -R www:www /www
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
-mv conf/nginx.conf /etc/nginx
-cp tools/index.html /www/index.html
-openrc && touch /run/openrc/softlevel
+cp -r conf/nginx.conf /etc/nginx/nginx.conf
+cp -r www/index.html /www/index.html
+openrc
+touch /run/openrc/softlevel
 rc-service nginx start
 rc-update add nginx default
