4d24c44767e33a4d631974ec5beabb59a896520f
MiguelTolino <mmateo-t@student.42madrid.com>
2022-12-03 22:32:18 +0000

    [#7] Add TLS to nginx

--- srcs/requirements/nginx/tools/nginx_conf.sh
+++ srcs/requirements/nginx/tools/nginx_conf.sh
@@ -6,22 +6,25 @@
 #    By: mmateo-t <mmateo-t@student.42madrid>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/27 13:04:47 by mmateo-t          #+#    #+#              #
-#    Updated: 2022/12/01 17:37:58 by mmateo-t         ###   ########.fr        #
+#    Updated: 2022/12/03 22:28:59 by mmateo-t         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 DOMAIN=mmateo.42.es
 
-apk update && apk upgrade && apk add --no-cache openrc nginx
+apk update && apk upgrade && apk add --no-cache openrc nginx openssl
 adduser -D -g 'www' www
 mkdir /www
 chown -R www:www /var/lib/nginx
 chown -R www:www /www
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
 cp -f conf/nginx.conf /etc/nginx/nginx.conf
-mkdir /www/$DOMAIN
+mkdir -p /www/$DOMAIN 
 cp -r conf/www/* /www/$DOMAIN
 openrc
 touch /run/openrc/softlevel
-rc-service nginx start
 rc-update add nginx default
+mkdir /etc/nginx/certificate
+cd /etc/nginx/certificate
+openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out nginx-certificate.crt -keyout nginx.key \
+-subj "/C=ES/ST=Madrid/L=Madrid/O=42Madrid/OU=42Madrid/CN=mmateo.42.es"
