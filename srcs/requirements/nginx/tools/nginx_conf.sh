9811d780414096378034f7824ee5929faf58ef67
mmateo-t <mmateo-t@c2r6s6.42madrid.com>
2022-12-15 21:24:11 +0100

    Install php7, download wordpress and enable nginx php support [#5 #4]

--- srcs/requirements/nginx/tools/nginx_conf.sh
+++ srcs/requirements/nginx/tools/nginx_conf.sh
@@ -12,18 +12,27 @@
 
 DOMAIN=mmateo.42.es
 
+# Install dependencies
 apk update && apk upgrade && apk add --no-cache openrc nginx openssl
+
+# Create user and group
 adduser -D -g 'www' www
 mkdir /www
 chown -R www:www /var/lib/nginx
 chown -R www:www /www
+
+# Set a new nginx configuration
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
 cp -f conf/nginx.conf /etc/nginx/nginx.conf
 mkdir -p /www/$DOMAIN 
 cp -r conf/www/* /www/$DOMAIN
+
+# Configurate nginx as default
 openrc
 touch /run/openrc/softlevel
 rc-update add nginx default
+
+# Generates SSL certificates
 mkdir /etc/nginx/certificate
 cd /etc/nginx/certificate
 openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out nginx-certificate.crt -keyout nginx.key \
