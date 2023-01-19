83458de5d1d80d0705e96da24cda3e2de74e58f0
mmateo-t <mmateo-t@student.42madrid.com>
2023-01-20 00:11:05 +0100

    MariaDB - Wordpress are connected

--- srcs/requirements/mariadb/conf/mariadb_script.sh
+++ srcs/requirements/mariadb/conf/mariadb_script.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
 
-rc-service mariadb start
+if [ ! -d /run/mysqld ]; then
+	mkdir -p /run/mysqld
+fi
 
-mysql -u root < conf/setup.sql
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-rc-service mariadb restart
+chown -R mysql:mysql /run/mysqld
+
+if [ ! -d /var/lib/mysql/mysql ]; then
+	chown -R mysql:mysql /var/lib/mysql
+	mysql_install_db --user=mysql --ldata=/var/lib/mysql
+	cat << EOF > "tmp.sql"
+USE mysql;
+FLUSH PRIVILEGES;
+GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION ;
+FLUSH PRIVILEGES ;
+DROP DATABASE IF EXISTS test ;
+DELETE FROM mysql.user WHERE User='';
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS wordpress CHARACTER SET utf8 COLLATE utf8_general_ci ;
+GRANT ALL ON wordpress.* TO 'wpuser'@'%' IDENTIFIED BY 'password' ;
+GRANT ALL ON wordpress.* TO 'wpuser'@'localhost' IDENTIFIED BY 'password' ;
+FLUSH PRIVILEGES ;
+EOF
+	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < tmp.sql
+	rm -f tmp.sql
+	echo "bind-address=0.0.0.0" >> /etc/my.cnf.d/mariadb-server.cnf
+else
+	chown -R mysql:mysql /var/lib/mysql
+fi
+
+exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0
--- srcs/requirements/mariadb/conf/setup.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE wordpress;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost' IDENTIFIED BY 'password';
-FLUSH PRIVILEGES;
-EXIT
