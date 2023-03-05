b5ec6e63a66cb94fa46ce5a0c8e85ad308b1aade
mmateo-t <mmateo-t@student.42madrid.com>
2023-03-05 14:02:13 +0100

    Add new virtuall host - Birdwatching

--- srcs/requirements/wordpress/tools/wp-script.sh
+++ srcs/requirements/wordpress/tools/wp-script.sh
@@ -25,9 +25,9 @@ sed -i 's/'"'SECURE_AUTH_SALT',".*"'put your unique phrase here'"'/'"'SECURE_AUT
 sed -i 's/'"'LOGGED_IN_SALT',".*"'put your unique phrase here'"'/'"'LOGGED_IN_SALT', '""$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)""'"'/g' /var/www/wordpress/wp-config.php;
 sed -i 's/'"'NONCE_SALT',".*"'put your unique phrase here'"'/'"'NONCE_SALT', '""$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)""'"'/g' /var/www/wordpress/wp-config.php;
 
-fi
 wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_USER --admin_password=$WP_PWD --admin_email=$WP_EMAIL --skip-email --allow-root --path=/var/www/wordpress
 wp user create $LOGIN $EMAIL --role=author --user_pass=$WP_PWD --allow-root --path=/var/www/wordpress
 wp theme install inspiro --activate --allow-root --path=/var/www/wordpress
+fi
 
 exec php-fpm7 -F
